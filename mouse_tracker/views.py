f3b24dfe3237b9aa606ed162cbb1461afd45bf7d
Vitor Aleixo <dittgp@gmail.com>
2019-09-04 23:15:05 -0300

    View com video do Rato

--- mouse_tracker/views.py
+++ mouse_tracker/views.py
@@ -6,50 +6,40 @@ import threading
 import gzip
 import cv2
 
-subtractor = cv2.createBackgroundSubtractorMOG2(history = 100,	varThreshold = 8,	detectShadows = False )
+subtractor = cv2.createBackgroundSubtractorMOG2( detectShadows = False)
 
 # Create your views here.
 class VideoCamera(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(0)
-        #self.video = cv2.VideoCapture("video.mp4")
-        (self.grabbed, self.frame) = self.video.read()
-        threading.Thread(target=self.update, args=()).start()
+        self.video = cv2.VideoCapture("video.mp4")
 
     def __del__(self):
         self.video.release()
 
     def get_frame(self):
-        image = self.frame
-        #Flip Image Horizontally
-        image = cv2.flip(image, 1)
+        success, image = self.video.read()
+ 
+        #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
     def update(self):
         while True:
-            (self.grabbed, self.frame) = self.video.read()
-
-    def desenharCaixa():
-        return False
-
-
+            self.grabbed, self.frame = self.video.read()
+  
 cam = VideoCamera()
-
 
 def gen(camera):
     while True:
         frame = cam.get_frame()
+     
         yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
-
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def livefe(request):
     try:
-        return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
-        #return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
-    except:  # This is bad! replace it with proper handling
+        return StreamingHttpResponse(gen(VideoCamera()),content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  
         pass
 
 def stream(request):
@@ -57,8 +47,3 @@ def stream(request):
         'something': "something"
     }
     return render(request, 'mouse_tracker/index.html', context)
-
-
-## Simple Tracking
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
