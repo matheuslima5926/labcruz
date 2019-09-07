87c448df08541881738e42d2fb5e982f7f787c42
Vitor Aleixo <dittgp@gmail.com>
2019-09-06 21:04:58 -0300

    Tracking com retangulo

--- mouse_tracker/views.py
+++ mouse_tracker/views.py
@@ -7,7 +7,7 @@ import gzip
 import cv2
 
 subtractor = cv2.createBackgroundSubtractorMOG2( detectShadows = False)
-
+test = 0
 # Create your views here.
 class VideoCamera(object):
     def __init__(self):
@@ -18,8 +18,32 @@ class VideoCamera(object):
 
     def get_frame(self):
         success, image = self.video.read()
- 
-        #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
+
+    #APLICA O SUBTRACTOR
+        mask = subtractor.apply(image)
+
+    #REDUZ O RUIDO COM MORPHOLOGY
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
+
+    #PEGA APENAS O CONTORNO
+        mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, None)
+
+    #DILATA PARA FICAR MAIS FACIL VER (Quanto maior o unumero de iteracoes, maior a dilatacao)
+    #Poderia colocar o numero de iteracoes editavel
+        mask = cv2.dilate(mask, None, iterations=12)
+
+    #CAPTURA OS CONTORNOS
+        (contours, hierarchy) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    
+    #DESENHA OS CONTORNOS
+        for c in contours:
+    #Aqui da pra fazer o tratamento de ruido do vídeo
+        
+            if cv2.contourArea(c) > 2000 and cv2.contourArea(c) < 50000:
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                continue
+    #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
