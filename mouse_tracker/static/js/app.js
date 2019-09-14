f3bee29e722359dd2e41e9bc3c6e158528285d47
Gabriel Taques <gabrielpctaques@gmail.com>
2019-09-14 13:00:27 -0300

    Selection box

--- mouse_tracker/static/js/app.js
+++ mouse_tracker/static/js/app.js
@@ -1,1 +1,42 @@
-console.log("Javascript is liked!");
+var canvas = document.getElementById('myCanvas'),
+    ctx = canvas.getContext('2d'),
+    rect = {},
+    drag = false;
+
+function init() {
+  canvas.addEventListener('mousedown', mouseDown, false);
+  canvas.addEventListener('mouseup', mouseUp, false);
+  canvas.addEventListener('mousemove', mouseMove, false);
+}
+
+function mouseDown(e) {
+  rect.startX = e.pageX - this.offsetLeft;
+  rect.startY = e.pageY - this.offsetTop;
+  drag = true;
+}
+
+function mouseUp() {
+  drag = false;
+    //ctx.clearRect(0,0,canvas.width,canvas.height);
+    console.log(canvas.width, canvas.height);
+
+    console.log("inicial X: " + rect.startX);
+    console.log("inicial Y: " + rect.startY);
+    console.log("rect Width: " + rect.w);
+    console.log("rect Height: " + rect.h);
+}
+function mouseMove(e) {
+  if (drag) {
+    rect.w = (e.pageX - this.offsetLeft) - rect.startX;
+    rect.h = (e.pageY - this.offsetTop) - rect.startY ;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    draw();
+  }
+}
+
+function draw() {
+    ctx.setLineDash([6]);
+  ctx.strokeRect(rect.startX, rect.startY, rect.w, rect.h);
+}
+
+init();
