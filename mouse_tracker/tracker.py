d4c78223282cb6ed6c7c95170e98de9be9791e14
Gabriel Taques <gabrielpctaques@gmail.com>
2019-09-22 18:39:25 -0300

    Algoritmo novo

--- mouse_tracker/tracker.py
+++ mouse_tracker/tracker.py
@@ -11,8 +11,10 @@ subtractor = cv2.createBackgroundSubtractorMOG2( detectShadows = False)
 test = 0
 class Tracker(object):
     roi = None
+    backSub = cv2.createBackgroundSubtractorKNN()
     def __init__(self, videoPath=None, roi=None):
         self.video = cv2.VideoCapture(videoPath)
+
         if roi:
             print("Atribuindo ROI!")
             self.roi = roi
@@ -27,38 +29,64 @@ class Tracker(object):
     """
     def get_frame(self):
         if self.roi:
-            success, image = self.video.read()
-            image_delimited = image[int(self.roi[1]):int(self.roi[1] + self.roi[3]), int(self.roi[0]):int(self.roi[0] + self.roi[2])]
+            ret, frame = self.video.read()
+
+            image_delimited = frame[int(self.roi[1]):int(self.roi[1] + self.roi[3]), int(self.roi[0]):int(self.roi[0] + self.roi[2])]
             
-            #APLICA O SUBTRACTOR
-            mask = subtractor.apply(image_delimited)
+            if frame is None:
+                return
+            
+            #aplica a subtração
+            fgMask = self.backSub.apply(image_delimited)
+            contours, hierarchy = cv2.findContours(fgMask.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
-            #REDUZ O RUIDO COM MORPHOLOGY
-            mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
-
-            #PEGA APENAS O CONTORNO
-            mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, None)
-
-            #DILATA PARA FICAR MAIS FACIL VER (Quanto maior o unumero de iteracoes, maior a dilatacao)
-            #Poderia colocar o numero de iteracoes editavel
-            mask = cv2.dilate(mask, None, iterations=12)
-
-            #CAPTURA OS CONTORNOS
-            (contours, hierarchy) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-            #DESENHA OS CONTORNOS
+            #mostra o numero de frames e mais umas coisaradas
+            # cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+            #         cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            #desenha o retangulo
             for c in contours:
-            #Aqui da pra fazer o tratamento de ruido do vídeo
-
-                if cv2.contourArea(c) > 2000 and cv2.contourArea(c) < 50000:
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(image_delimited, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                if cv2.contourArea(c) < 500:
                     continue
-            #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
-            ret, jpeg = cv2.imencode('.jpg', image)
+                (x, y, w, h) = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                continue
+
+            ret, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
 
+        # if self.roi:
+        #     success, image = self.video.read()
+        #     image_delimited = image[int(self.roi[1]):int(self.roi[1] + self.roi[3]), int(self.roi[0]):int(self.roi[0] + self.roi[2])]
+            
+        #     #APLICA O SUBTRACTOR
+        #     mask = subtractor.apply(image_delimited)
 
+        #     #REDUZ O RUIDO COM MORPHOLOGY
+        #     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
+
+        #     #PEGA APENAS O CONTORNO
+        #     mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, None)
+
+        #     #DILATA PARA FICAR MAIS FACIL VER (Quanto maior o unumero de iteracoes, maior a dilatacao)
+        #     #Poderia colocar o numero de iteracoes editavel
+        #     mask = cv2.dilate(mask, None, iterations=12)
+
+        #     #CAPTURA OS CONTORNOS
+        #     (contours, hierarchy) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        #     #DESENHA OS CONTORNOS
+        #     for c in contours:
+        #     #Aqui da pra fazer o tratamento de ruido do vídeo
+
+        #         if cv2.contourArea(c) > 2000 and cv2.contourArea(c) < 50000:
+        #             (x, y, w, h) = cv2.boundingRect(c)
+        #             cv2.rectangle(image_delimited, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        #             continue
+        #     #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
+        #     ret, jpeg = cv2.imencode('.jpg', image)
+        #     return jpeg.tobytes()
+
+        
         else:
             print("roi nao selecionado")
             success, image = self.video.read()
