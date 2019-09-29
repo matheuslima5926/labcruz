9efb81815edc3a2e4e1ca133fc57b634a2f6ff5b
Vitor Aleixo <dittgp@gmail.com>
2019-09-28 21:34:55 -0300

    Tracking com Dados

--- mouse_tracker/tracker.py
+++ mouse_tracker/tracker.py
@@ -30,13 +30,21 @@ class Tracker(object):
     isDireita = False
     isEsquerda = False
     isCentro = True
-
+    tempoTotalCima = 0.0
+    tempoTotalBaixo = 0.0
+    tempoTotalEsquerda = 0.0
+    tempoTotalDireita = 0.0
+    tempoTotalCentro = 0.0
+    totalBracosAbertos = 0
+    totalBracosFechados = 0
+    totalCruzamentos = 0
+    timerStartado = 0
     def __init__(self, videoPath=None, roi=None):
         self.video = cv2.VideoCapture(videoPath)
         fps = self.video.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
         frame_count = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
         duration = frame_count/fps
-
+        
         print('fps = ' + str(fps))
         print('number of frames = ' + str(frame_count))
         print('duration (S) = ' + str(duration))
@@ -115,7 +123,24 @@ class Tracker(object):
                 if self.minRangeY <= cY <= self.maxRangeY and self.minRangeX <= cX <= self.maxRangeX:
                     self.confirmCentro += 1
                     if self.confirmCentro == 3:
-                        # print(time.clock())
+                        self.totalCruzamentos += 1
+
+                        #CAPTURA A QUANTIDADE DE FRAMES DO VIDEO NESSE MOMENTO, SUBTRAI DA QUANTIDADE DE FRAMES QUE FOI STARTADO PELA ULTIMA VEZ EM ALGUM BRACO, E DIVIDE PELA 
+                        #QTD DE FPS, E AO LOCALIZAR O BRACO QUE O RATO ESTAVA, ATRIBUI E SOMA NA VARIAVEL
+                        if(self.confirmBaixo >= 2):
+                            self.tempoTotalBaixo += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmEsquerda >= 3):
+                             self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmCima >= 3):
+                            self.tempoTotalCima += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmDireita >= 3):
+                            self.tempoTotalDireita += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        
+                        timer = 0
+                        if(timer == 0):
+                            self.timerStartado = int(self.video.get(cv2.CAP_PROP_POS_FRAMES))
+                            timer = 1
+                            
                         self.isCentro = True
                         self.isBaixo = False
                         self.isCima = False
@@ -125,13 +150,29 @@ class Tracker(object):
                         self.confirmDireita = 0
                         self.confirmEsquerda = 0
                         self.confirmBaixo = 0
-                        # print("Centro")
                         # self.triggerTimer("centro")
 
                 elif cY > self.centerY + 30 and self.minRangeX <= cX <= self.maxRangeX:
                     self.confirmBaixo += 1
                     if self.confirmBaixo == 2:
-                        # print(time.clock())
+                        
+                        #CAPTURA A QUANTIDADE DE FRAMES DO VIDEO NESSE MOMENTO, SUBTRAI DA QUANTIDADE DE FRAMES QUE FOI STARTADO PELA ULTIMA VEZ EM ALGUM BRACO, E DIVIDE PELA 
+                        #QTD DE FPS, E AO LOCALIZAR O BRACO QUE O RATO ESTAVA, ATRIBUI E SOMA NA VARIAVEL
+                        self.totalBracosAbertos += 1
+                        if(self.confirmCentro >= 3):
+                            self.tempoTotalCentro += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmEsquerda >= 3):
+                             self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmCima >= 3):
+                            self.tempoTotalCima += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmDireita >= 3):
+                            self.tempoTotalDireita += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                            
+                        timer = 0
+                        if(timer == 0):
+                            self.timerStartado = int(self.video.get(cv2.CAP_PROP_POS_FRAMES))
+                            timer = 1
+                            
                         self.isBaixo = True
                         self.isCima = False
                         self.isDireita = False
@@ -141,13 +182,28 @@ class Tracker(object):
                         self.confirmDireita = 0
                         self.confirmEsquerda = 0
                         self.confirmCentro = 0
-                        # self.triggerTimer("centro")
                         # print("Baixo")
 
                 elif cY < self.centerY - 30 and self.minRangeX <= cX <= self.maxRangeX:
                     self.confirmCima += 1
                     if self.confirmCima == 3:
-                        # print(time.clock())
+                        self.totalBracosAbertos += 1
+                        
+                        #CAPTURA A QUANTIDADE DE FRAMES DO VIDEO NESSE MOMENTO, SUBTRAI DA QUANTIDADE DE FRAMES QUE FOI STARTADO PELA ULTIMA VEZ EM ALGUM BRACO, E DIVIDE PELA 
+                        #QTD DE FPS, E AO LOCALIZAR O BRACO QUE O RATO ESTAVA, ATRIBUI E SOMA NA VARIAVEL
+                        if(self.confirmCentro >= 3):
+                            self.tempoTotalCentro += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmEsquerda >= 3):
+                             self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado )  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmBaixo >= 2):
+                            self.tempoTotalBaixo += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmDireita >= 3):
+                            self.tempoTotalDireita += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        
+                        timer = 0
+                        if(timer == 0):
+                            self.timerStartado = int(self.video.get(cv2.CAP_PROP_POS_FRAMES))
+                            timer = 1
                         self.isCima = True
                         self.isBaixo = False
                         self.isDireita = False
@@ -157,13 +213,28 @@ class Tracker(object):
                         self.confirmDireita = 0
                         self.confirmEsquerda = 0
                         self.confirmCentro = 0
-                        # self.triggerTimer("centro")
                         # print("Cima")
 
                 elif cX > self.centerX + 30 and self.minRangeY <= cY <= self.maxRangeY:
                     self.confirmDireita += 1
                     if self.confirmDireita == 3:
-                        # print(time.clock())
+                        self.totalBracosFechados += 1
+                        
+                        #CAPTURA A QUANTIDADE DE FRAMES DO VIDEO NESSE MOMENTO, SUBTRAI DA QUANTIDADE DE FRAMES QUE FOI STARTADO PELA ULTIMA VEZ EM ALGUM BRACO, E DIVIDE PELA 
+                        #QTD DE FPS, E AO LOCALIZAR O BRACO QUE O RATO ESTAVA, ATRIBUI E SOMA NA VARIAVEL
+                        if(self.confirmCentro >= 3):
+                            self.tempoTotalCentro += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmEsquerda >= 3):
+                             self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmBaixo >= 2):
+                            self.tempoTotalBaixo += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)   / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmCima >= 3):
+                            self.tempoTotalCima += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerStartado)   / self.video.get(cv2.CAP_PROP_FPS)
+                        
+                        timer = 0
+                        if(timer == 0):
+                            self.timerStartado = int(self.video.get(cv2.CAP_PROP_POS_FRAMES))
+                            timer = 1
                         self.isDireita = True
                         self.isBaixo = False
                         self.isCima = False
@@ -173,13 +244,28 @@ class Tracker(object):
                         self.confirmBaixo = 0
                         self.confirmEsquerda = 0
                         self.confirmCentro = 0
-                        # self.triggerTimer("centro")
                         # print("Direita")
                 
                 elif cX < self.centerX - 30 and self.minRangeY <= cY <= self.maxRangeY:
                     self.confirmEsquerda += 1
                     if self.confirmEsquerda == 3:
-                        # print(time.clock())
+                        self.totalBracosFechados += 1
+                        
+                        #CAPTURA A QUANTIDADE DE FRAMES DO VIDEO NESSE MOMENTO, SUBTRAI DA QUANTIDADE DE FRAMES QUE FOI STARTADO PELA ULTIMA VEZ EM ALGUM BRACO, E DIVIDE PELA 
+                        #QTD DE FPS, E AO LOCALIZAR O BRACO QUE O RATO ESTAVA, ATRIBUI E SOMA NA VARIAVEL
+                        if(self.confirmCentro >= 3): 
+                            self.tempoTotalCentro += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmEsquerda >= 3):
+                             self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmBaixo >= 2):
+                            self.tempoTotalBaixo += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                        elif(self.confirmCima >= 3):
+                            self.tempoTotalCima +=  (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerCima) / self.video.get(cv2.CAP_PROP_FPS)
+                         
+                        timer = 0
+                        if(timer == 0):
+                            self.timerStartado = int(self.video.get(cv2.CAP_PROP_POS_FRAMES))
+                            timer = 1   
                         self.isEsquerda = True
                         self.isBaixo = False
                         self.isCima = False
@@ -188,48 +274,38 @@ class Tracker(object):
                         self.confirmBaixo = 0
                         self.confirmDireita = 0
                         self.confirmCentro = 0
-                        # self.triggerTimer("centro")
                         # print("Esquerda")
-
+                  
                 cv2.rectangle(image_delimited, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 
                 continue
-            # print(time.clock() - start_time, "seconds")
+            
+            # SE A QUANTIDADE DE FRAMES ATUAL FOR IGUAL A QUANTIDADE DE FRAMES FINAL DO VIDEO, SETA O UTLIMO TIMER NA ULTIMA VARIAVEL
+            # POR EXEMPLO, SE O RATO PAROU NO BRACO DE BAIXO POR ULTIMO, AO ENTRAR NESSA CONDICAO, ADICIONA O VALOR DO TIMER NA VARIAVEL DE BAIXO
+            if(int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) == int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))):
+                if(self.confirmCentro >= 3): 
+                    self.tempoTotalCentro += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                elif(self.confirmDireita >= 3):
+                    self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                elif(self.confirmEsquerda >= 3):
+                    self.tempoTotalEsquerda += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                elif(self.confirmBaixo >= 2):
+                    self.tempoTotalBaixo += (int(self.video.get(cv2.CAP_PROP_POS_FRAMES))  - self.timerStartado)  / self.video.get(cv2.CAP_PROP_FPS)
+                elif(self.confirmCima >= 3):
+                    self.tempoTotalCima +=  (int(self.video.get(cv2.CAP_PROP_POS_FRAMES)) - self.timerCima) / self.video.get(cv2.CAP_PROP_FPS)
+            
+            # PRINTAR OS VALORES NA TELA    
+            cv2.putText(frame,'Timer Centro: ' +  str(round(self.tempoTotalCentro,2)), (35, 35), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Timer Cima: ' +  str(round(self.tempoTotalCima,2)), (35, 55), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Timer Direita: ' +  str(round(self.tempoTotalDireita,2)), (35, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Timer Baixo: ' +  str(round(self.tempoTotalBaixo,2)), (35, 95), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Timer Esquerda: ' +  str(round(self.tempoTotalEsquerda,2)), (35, 115), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Total Cruzamentos: ' +  str(round(self.totalCruzamentos,2)), (35, 135), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Total Bracos Abertos: ' +  str(round(self.totalBracosAbertos,2)), (35, 155), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            cv2.putText(frame,'Total Bracos Fechados: ' +  str(round(self.totalBracosFechados,2)), (35, 175), cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+            
             ret, jpeg = cv2.imencode('.jpg', frame)
             return jpeg.tobytes()
-
-        # if self.roi:
-        #     success, image = self.video.read()
-        #     image_delimited = image[int(self.roi[1]):int(self.roi[1] + self.roi[3]), int(self.roi[0]):int(self.roi[0] + self.roi[2])]
-            
-        #     #APLICA O SUBTRACTOR
-        #     mask = subtractor.apply(image)
-
-        #     #REDUZ O RUIDO COM MORPHOLOGY
-        #     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, None)
-
-        #     #PEGA APENAS O CONTORNO
-        #     mask = cv2.morphologyEx(mask, cv2.MORPH_GRADIENT, None)
-
-        #     #DILATA PARA FICAR MAIS FACIL VER (Quanto maior o unumero de iteracoes, maior a dilatacao)
-        #     #Poderia colocar o numero de iteracoes editavel
-        #     mask = cv2.dilate(mask, None, iterations=12)
-
-        #     #CAPTURA OS CONTORNOS
-        #     (contours, hierarchy) = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-        #     #DESENHA OS CONTORNOS
-        #     for c in contours:
-        #     #Aqui da pra fazer o tratamento de ruido do vídeo
-
-        #         if cv2.contourArea(c) > 2000 and cv2.contourArea(c) < 50000:
-        #             (x, y, w, h) = cv2.boundingRect(c)
-        #             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        #             continue
-        #     #SE image FOR NULO, RETORNA UM .jpg VAZIO (Se nao, quando acaba o video da pau em tudo)
-        #     ret, jpeg = cv2.imencode('.jpg', image)
-        #     return jpeg.tobytes()
-
         
         else:
             print("roi nao selecionado")
